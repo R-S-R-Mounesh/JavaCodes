7d276c4623e44146076d2171e9c5446743886d45
R-S-R-Mounesh <131319795+R-S-R-Mounesh@users.noreply.github.com>
2023-11-06 15:17:12 +0530

    Add files via upload

---
+++ Pyramid6.java
@@ -0,0 +1,19 @@
+public class Demo{
+	public static void main(String[]args) {
+	
+		 int rows = 9;
+		 int count =1;
+	        for (int i = 1; i <= rows; i++) {
+	        	  for (int k = i; k <=rows; k++) {
+		                System.out.print(" ");
+		            }
+	           
+	            for (int k = 1; k <=i; k++) {
+	                System.out.print("*"+" ");
+	            }
+	            System.out.println();
+	           count++;
+		}
+	}
+}
+
