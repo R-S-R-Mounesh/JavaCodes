08660ba83339b687e98c031c3dbc379a36b01f64
R-S-R-Mounesh <131319795+R-S-R-Mounesh@users.noreply.github.com>
2023-11-06 15:11:48 +0530

    Add files via upload

---
+++ Pyramid2.java
@@ -0,0 +1,17 @@
+
+
+public class Demo{
+	public static void main(String[]args) {
+	
+		 int rows = 9;
+	        for (int i = rows; i >=1; i--) {
+	            for (int j = 1; j <= rows-i; j++) {
+	                System.out.print(" ");
+	            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+	                System.out.print(i);
+	            }
+	            System.out.println();
+		}
+	}
+}
